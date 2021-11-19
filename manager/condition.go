efe2364b7c5d7d8b440d87f830c814e24ca8e74f
RicheyJang <773602577@qq.com>
2021-11-19 15:57:06 +0800

    better status

--- manager/condition.go
+++ manager/condition.go
@@ -9,8 +9,13 @@ type PluginCondition struct {
 	NormalCmd  [][]string // 普通用户专用命令
 	SuperCmd   [][]string // 超级用户专用命令
 
-	disabled bool       // 插件是否启用，默认false：启用
+	disabled bool       // 插件是否禁用（全局，针对所有用户），默认false：启用
 	schedule *cron.Cron // 定时任务结构
+}
+
+// Status 插件全局启用状态
+func (c PluginCondition) Status() bool {
+	return !c.disabled
 }
 
 // Enabled 启用插件
@@ -27,7 +32,7 @@ func (c *PluginCondition) Disabled() {
 
 // StartCron 开始所有定时任务
 func (c *PluginCondition) StartCron() {
-	if c.schedule != nil && !c.disabled { // 已有定时任务结构且插件启用
+	if c.schedule != nil && c.Status() { // 已有定时任务结构且插件启用
 		c.schedule.Start()
 	}
 }
