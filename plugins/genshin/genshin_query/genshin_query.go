ba3042feeba3f6d13cd583610f610e36a864af70
乐乐大仙啊 <484491598@qq.com>
2022-03-03 23:12:12 +0800

    查询修正

--- plugins/genshin/genshin_query/genshin_query.go
+++ plugins/genshin/genshin_query/genshin_query.go
@@ -20,19 +20,22 @@ func Query(uid string, cookie string) (string, *genshin_public.GenshinDailyNote,
 	}
 	now := time.Now()
 	msg := fmt.Sprintf(
-		"用户:%s\n[用户树脂:%d/%d]\n[树脂回满时间:%s]\n[用户洞天宝钱:%d/%d]\n[用户派遣%d/%d]",
+		"用户:%s\n[用户树脂:%d/%d]\n[树脂回满时间:%s]\n[用户洞天宝钱:%d/%d]\n[宝钱回满时间:%s]\n[用户派遣%d/%d]",
 		uid,
 		dailyNote.CurrentResin,
 		dailyNote.MaxResin,
-		addTime(now, dailyNote.HomeCoinRecoveryTime), dailyNote.CurrentHomeCoin, dailyNote.MaxHomeCoin, dailyNote.CurrentExpeditionNum, dailyNote.MaxExpeditionNum)
+		addTime(now, dailyNote.ResinRecoveryTime),
+		dailyNote.CurrentHomeCoin, dailyNote.MaxHomeCoin,
+		addTime(now, dailyNote.HomeCoinRecoveryTime),
+		dailyNote.CurrentExpeditionNum, dailyNote.MaxExpeditionNum)
 	return msg, dailyNote, err
 }
 func addTime(now time.Time, duration string) string {
 	add, err := strconv.Atoi(duration)
 	if err != nil {
 		after := now.Add(8 * 160 * time.Minute)
-		return after.String()
+		return fmt.Sprintf("%04d-%02d-%02d %02dH-%02dM", after.Year(), after.Month(), after.Day(), after.Hour(), after.Minute())
 	}
-	after := now.Add(time.Duration(time.Second * time.Duration(add/10)))
-	return after.String()
+	after := now.Add(time.Duration(time.Second * time.Duration(int(add))))
+	return fmt.Sprintf("%04d-%02d-%02d %02d小时%02d分", after.Year(), after.Month(), after.Day(), after.Hour(), after.Minute())
 }
