57c6a119f5ae49454b8f28471d8643be129e418b
乐乐大仙啊 <484491598@qq.com>
2022-03-03 22:27:03 +0800

    查询修正

--- plugins/genshin/init.go
+++ plugins/genshin/init.go
@@ -1,8 +1,8 @@
 package genshin
 
 import (
-	_ "github.com/RicheyJang/PaimengBot/plugins/genshin/genshin_draw"
-	_ "github.com/RicheyJang/PaimengBot/plugins/genshin/genshin_resource"
-	_ "github.com/RicheyJang/PaimengBot/plugins/genshin/genshin_sign"
 	_ "github.com/RicheyJang/PaimengBot/plugins/genshin/genshin_cookie"
+	_ "github.com/RicheyJang/PaimengBot/plugins/genshin/genshin_draw"
+	_ "github.com/RicheyJang/PaimengBot/plugins/genshin/genshin_query"
+	_ "github.com/RicheyJang/PaimengBot/plugins/genshin/genshin_resource"
 )
