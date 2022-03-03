d56302593ba88c4b2271ddd2f284d4d59e307206
乐乐大仙啊 <484491598@qq.com>
2022-03-04 00:23:44 +0800

    签到

--- plugins/genshin/genshin_sign/sign.go
+++ plugins/genshin/genshin_sign/sign.go
@@ -3,8 +3,6 @@ package genshin_sign
 import (
 	"fmt"
 	"github.com/RicheyJang/PaimengBot/plugins/genshin/genshin_sign/sign_client"
-
-	"time"
 )
 
 func Sign(uid string, cookie string) (string, error) {
@@ -14,10 +12,10 @@ func Sign(uid string, cookie string) (string, error) {
 	gameRolesList := g.GetUserGameRoles(cookie)
 
 	for j := 0; j < len(gameRolesList); j++ {
-		time.Sleep(10 * time.Second)
+		//time.Sleep(10 * time.Second)
 		msg := ""
 		if g.Sign(cookie, gameRolesList[j]) {
-			time.Sleep(10 * time.Second)
+			//time.Sleep(10 * time.Second)
 			data := g.GetSignStateInfo(cookie, gameRolesList[j])
 			msg = fmt.Sprintf("UID:%v, 昵称:%v, 连续签到天数:%v. 签到成功.",
 				gameRolesList[j].UID, gameRolesList[j].Name, data.TotalSignDay)
