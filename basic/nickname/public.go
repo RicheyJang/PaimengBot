5c51d57f80df66dcfb359d437be1ea9a0186c91a
RicheyJang <773602577@qq.com>
2022-03-09 14:27:41 +0800

    feat: limit length of nickname

--- basic/nickname/public.go
+++ basic/nickname/public.go
@@ -1,12 +1,28 @@
 package nickname
 
-import "github.com/RicheyJang/PaimengBot/basic/dao"
+import (
+	"github.com/RicheyJang/PaimengBot/basic/dao"
+	"github.com/RicheyJang/PaimengBot/utils"
+)
 
 // GetNickname 获取用户昵称
 func GetNickname(userID int64, defaultName string) string {
 	var user dao.UserSetting
 	res := proxy.GetDB().Select("nickname").Take(&user, userID)
 	if res.RowsAffected > 0 {
+		// 检查昵称长度
+		max := int(proxy.GetConfigInt64("max"))
+		if utils.StringRealLength(user.Nickname) > max {
+			if max > 0 {
+				user.Nickname = string([]rune(user.Nickname)[:max])
+			} else {
+				return defaultName
+			}
+		}
+		// 长度为0，返回默认
+		if len(user.Nickname) == 0 {
+			return defaultName
+		}
 		return user.Nickname
 	}
 	return defaultName
