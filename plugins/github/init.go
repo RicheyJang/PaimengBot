d533e03f91d6e8187ac4881b47f2fd27554b4e00
RicheyJang <773602577@qq.com>
2022-06-17 20:43:06 +0800

    fix: better structure

--- plugins/github/init.go
+++ plugins/github/init.go
@@ -1,27 +1,27 @@
 package github
 
 import (
-	"bytes"
-	"encoding/base64"
+	"net/url"
+	"strconv"
+
 	"github.com/RicheyJang/PaimengBot/manager"
 	"github.com/RicheyJang/PaimengBot/utils"
 	"github.com/RicheyJang/PaimengBot/utils/client"
 	log "github.com/sirupsen/logrus"
 	zero "github.com/wdvxdr1123/ZeroBot"
 	"github.com/wdvxdr1123/ZeroBot/message"
-	"io"
 )
 
 var proxy *manager.PluginProxy
 var info = manager.PluginInfo{
 	Name: "GitHub查询",
-	Usage: `
-查询某个仓库
+	Usage: `查询某个Github仓库的相关信息
 用法：
-	github[查询名称]
+	github [仓库名称]
 例子：
-	github RicheyJang/PaimengBot
-`,
+	github RicheyJang/PaimengBot`,
+	SuperUsage: `config-plugin配置项：
+	github.maxresult：一次性最多返回多少个仓库信息`,
 	Classify: "一般功能",
 }
 
@@ -30,8 +30,8 @@ func init() {
 	if proxy == nil {
 		return
 	}
-	proxy.OnRegex("^github\\s*(.+)").SetBlock(true).SetPriority(4).Handle(handleReg)
-	proxy.AddConfig("maxResult", 2)
+	proxy.OnRegex(`^github\s+([\x00-\xff]+)$`).SetBlock(true).SetPriority(4).Handle(handleReg)
+	proxy.AddConfig("maxResult", 1)
 }
 
 const githubAPI = "https://api.github.com/search/repositories"
@@ -39,12 +39,17 @@ const githubAPI = "https://api.github.com/search/repositories"
 func handleReg(ctx *zero.Ctx) {
 	// 最大结果数
 	var maxResult = int(proxy.GetConfigInt64("maxResult"))
+	if maxResult <= 0 {
+		maxResult = 1
+	}
+	// 调用API
 	var c = client.NewHttpClient(nil)
-	target := utils.GetRegexpMatched(ctx)[1]
+	target := url.QueryEscape(utils.GetRegexpMatched(ctx)[1])
 	// 获取结果
-	json, errApi := c.GetGJson(githubAPI + "?q=" + target)
+	json, errApi := c.GetGJson(githubAPI + "?q=" + target + "&per_page=" + strconv.FormatInt(int64(maxResult), 10))
 	if errApi != nil {
 		log.Errorf("GitHubApi返回解析错误,err=%s", errApi.Error())
+		ctx.Send("失败了...")
 		return
 	}
 	if count := json.Get("total_count").Int(); count <= 0 {
@@ -54,7 +59,7 @@ func handleReg(ctx *zero.Ctx) {
 
 	var names []string
 	for i, result := range json.Get("items").Array() {
-		if i > maxResult {
+		if i >= maxResult {
 			break
 		}
 		names = append(names, result.Get("full_name").String())
@@ -63,34 +68,7 @@ func handleReg(ctx *zero.Ctx) {
 }
 
 func sendImageChain(ctx *zero.Ctx, fullNames []string) {
-	var messages []message.MessageSegment
 	for _, name := range fullNames {
-		messages = append(messages, getImage(name))                           // 图片
-		messages = append(messages, message.Text("https://github.com/"+name)) // 仓库地址
+		ctx.Send(message.Image("https://opengraph.githubassets.com/0/" + name))
 	}
-	ctx.SendChain(messages...)
-}
-
-// 获取image消息
-func getImage(fullName string) message.MessageSegment {
-	var c = client.NewHttpClient(nil)
-	var respImage, _ = c.Get("https://opengraph.githubassets.com/0/" + fullName)
-	// Base64
-	resultBuff := bytes.NewBuffer(nil) // 结果缓冲区
-	// 新建Base64编码器（Base64结果写入结果缓冲区resultBuff）
-	encoder := base64.NewEncoder(base64.StdEncoding, resultBuff)
-	// 将文件写入Base64编码器
-	_, err := io.Copy(encoder, respImage.Body)
-	if err != nil {
-		log.Warnf("localImage2Send base64 copy err: %v", err)
-		_ = encoder.Close()
-		return message.Text("[Error]生成图片失败")
-	}
-	// 结束Base64编码
-	err = encoder.Close()
-	if err != nil {
-		log.Warnf("localImage2Send base64 encode err: %v", err)
-		return message.Text("[Error]生成图片失败")
-	}
-	return message.Image("base64://" + resultBuff.String())
 }
