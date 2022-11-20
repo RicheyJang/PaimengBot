ee95d428e5c28fb88f0b62fb1f884898d8251642
RicheyJang <773602577@qq.com>
2022-11-20 12:31:45 +0800

    fix: mark unavailable plugins

--- plugins/genshin/init.go
+++ plugins/genshin/init.go
@@ -4,7 +4,7 @@ import (
 	_ "github.com/RicheyJang/PaimengBot/plugins/genshin/genshin_cookie"
 	_ "github.com/RicheyJang/PaimengBot/plugins/genshin/genshin_draw"
 	_ "github.com/RicheyJang/PaimengBot/plugins/genshin/genshin_query"
-	_ "github.com/RicheyJang/PaimengBot/plugins/genshin/genshin_record"
 	_ "github.com/RicheyJang/PaimengBot/plugins/genshin/genshin_sign"
+	// _ "github.com/RicheyJang/PaimengBot/plugins/genshin/genshin_record" // 已失效
 	// _ "github.com/RicheyJang/PaimengBot/plugins/genshin/genshin_resource" // 已失效
 )
